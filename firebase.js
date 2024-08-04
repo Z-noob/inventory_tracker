f8a9001d2d1b41d223a1bc4f9331c946eae7abeb
Z-noob <Zhenniwu5@gmail.com>
2024-08-04 14:52:52 -0400

    style

--- firebase.js
+++ firebase.js
@@ -6,22 +6,22 @@ import { getFirestore } from 'firebase/firestore'
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: NEXT_PUBLIC_FIREBASE_API_KEY,
-//   authDomain: NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-//   projectId: NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-//   storageBucket: NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: NEXT_PUBLIC_FIREBASE_APP_ID
-// };
 const firebaseConfig = {
-  apiKey: "AIzaSyAFOa0Aa-3ANaCvFXx-tFGINolEfkqx9fM",
-  authDomain: "inventory-management-5dd50.firebaseapp.com",
-  projectId: "inventory-management-5dd50",
-  storageBucket: "inventory-management-5dd50.appspot.com",
-  messagingSenderId: "166952853808",
-  appId: "1:166952853808:web:675013af050577bac1e8f5"
+  apiKey: NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: NEXT_PUBLIC_FIREBASE_APP_ID
 };
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAFOa0Aa-3ANaCvFXx-tFGINolEfkqx9fM",
+//   authDomain: "inventory-management-5dd50.firebaseapp.com",
+//   projectId: "inventory-management-5dd50",
+//   storageBucket: "inventory-management-5dd50.appspot.com",
+//   messagingSenderId: "166952853808",
+//   appId: "1:166952853808:web:675013af050577bac1e8f5"
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
